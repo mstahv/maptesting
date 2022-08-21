b5a22d636a5e3605596c346a603c2b726ea7300c
Matti Tahvonen <matti@vaadin.com>
2022-08-21 22:42:29 +0300

    Refactored Geolocation API usage into helper classes that implements pretty much the full spcification

--- src/main/java/com/example/application/MainView.java
+++ src/main/java/com/example/application/MainView.java
@@ -1,19 +1,35 @@
 package com.example.application;
 
+import com.example.application.geolocation.Geolocation;
+import com.example.application.geolocation.GeolocationOptions;
+import com.vaadin.flow.component.AbstractField;
+import com.vaadin.flow.component.AbstractSinglePropertyField;
 import com.vaadin.flow.component.ClientCallable;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.map.Map;
 import com.vaadin.flow.component.map.configuration.Coordinate;
 import com.vaadin.flow.component.map.configuration.View;
 import com.vaadin.flow.component.map.configuration.feature.MarkerFeature;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 @PageTitle("Map")
 @Route
 public class MainView extends VerticalLayout {
 
+    private Geolocation geolocation;
     private Map map = new Map();
 
     MarkerFeature myLocation = null;
@@ -39,29 +55,52 @@ public class MainView extends VerticalLayout {
             }
         });
 
-        // Ask permission to use the location adn pass
-        // location to the server
-        getElement().executeJs("""
-            var el = this;
-            // https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/watchPosition
-            navigator.geolocation.watchPosition(
-              position => {
-                el.$server.updateMyLocation(position.coords.latitude,position.coords.longitude);
-              },
-              error => {
-                 // those ever happen for the great developers :-)
-              },
-              {enableHighAccuracy: true, timeout: 5000, maximumAge: 1000 }
-            );
-        """);
+        Checkbox enableHighAccuracy = new Checkbox("High accuracy");
+        IntegerField timeout = new IntegerField("Timeout");
+        IntegerField maximumAge = new IntegerField("Max age");
+
+        List<AbstractField> abstractSinglePropertyFields = Arrays.asList(enableHighAccuracy, timeout, maximumAge);
+
+        Button button = new Button("Start tracking");
+        button.addClickListener(e -> {
+            if(geolocation == null) {
+                geolocation = Geolocation.watchPosition(
+                        event -> {
+                            System.out.println(Instant.ofEpochMilli(event.getTimestamp()) + ":" + event.getCoords());
+                            updateMyLocation(event.getCoords().getLatitude(), event.getCoords().getLongitude());
+                        },
+                        browserError -> {
+                            Notification.show("ERROR: " + browserError);
+                        },
+                        new GeolocationOptions(enableHighAccuracy.getValue(), timeout.getValue(), maximumAge.getValue())
+                );
+                button.setText("Stop tracking");
+                abstractSinglePropertyFields
+                        .forEach(c -> c.setEnabled(false));
+            } else {
+                geolocation.cancel();
+                geolocation = null;
+                button.setText("Start tracking");
+                abstractSinglePropertyFields
+                        .forEach(c -> c.setEnabled(true));
+            }
+        });
+
+        Button checkOnce = new Button("Check once", e -> {
+                Geolocation.watchPosition(
+                        event -> {
+                            System.out.println(Instant.ofEpochMilli(event.getTimestamp()) + ":" + event.getCoords());
+                            updateMyLocation(event.getCoords().getLatitude(), event.getCoords().getLongitude());
+                        },
+                        browserError -> {
+                            Notification.show("ERROR: " + browserError);
+                        },
+                        new GeolocationOptions(enableHighAccuracy.getValue(), timeout.getValue(), maximumAge.getValue())
+                );
+        });
+        add(new HorizontalLayout(button, checkOnce, enableHighAccuracy, timeout, maximumAge));
     }
 
-    /**
-     * Called by the browser on new geolocation updates
-     * @param lat latitude
-     * @param lon longitude
-     */
-    @ClientCallable
     private void updateMyLocation(double lat, double lon) {
         if(myLocation == null) {
             myLocation = new MarkerFeature();
@@ -71,4 +110,5 @@ public class MainView extends VerticalLayout {
         myLocation.setCoordinates(coordinate);
         map.setCenter(coordinate);
     }
+
 }
---
+++ src/main/java/com/example/application/geolocation/GeolocationEvent.java
@@ -0,0 +1,20 @@
+package com.example.application.geolocation;
+
+public class GeolocationEvent {
+    private GeolocationCoordinates coords;
+    private long timestamp;
+
+    /**
+     * @return geographic coordinates.
+     */
+    public GeolocationCoordinates getCoords() {
+        return coords;
+    }
+
+    /**
+     * @return the time when the geographic position of the device was acquired.
+     */
+    public long getTimestamp() {
+        return timestamp;
+    }
+}
